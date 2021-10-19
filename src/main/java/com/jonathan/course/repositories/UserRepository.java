9450add1d4b8663d7df4831e24da07a4b0e04b17
Jonathan Silva <jonathanfelsilva@gmail.com>
2021-10-19 15:40:16 -0300

    Criação do TestConfig e da dependência do UserRepository

---
+++ src/main/java/com/jonathan/course/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.jonathan.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jonathan.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+		
+	
+	
+	
+}
