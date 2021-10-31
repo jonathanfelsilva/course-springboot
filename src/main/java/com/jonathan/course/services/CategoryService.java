700c72f17585d65c698b3bd4afc7dded52c8ed35
Jonathan Silva <jonathanfelsilva@gmail.com>
2021-10-31 16:21:30 -0300

    Início da criação de Post, Put e Delete de produtos

--- src/main/java/com/jonathan/course/services/CategoryService.java
+++ src/main/java/com/jonathan/course/services/CategoryService.java
@@ -26,8 +26,8 @@ public class CategoryService {
 	}
 	
 	public Category findById(Long id) {
-		Optional<Category> order = repository.findById(id);
-		return order.get();
+		Optional<Category> category = repository.findById(id);
+		return category.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public Category insert(Category obj) {
--- src/main/java/com/jonathan/course/services/ProductService.java
+++ src/main/java/com/jonathan/course/services/ProductService.java
@@ -3,11 +3,17 @@ package com.jonathan.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.jonathan.course.entities.Product;
 import com.jonathan.course.repositories.ProductRepository;
+import com.jonathan.course.services.exceptions.DatabaseException;
+import com.jonathan.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -21,7 +27,39 @@ public class ProductService {
 	
 	public Product findById(Long id) {
 		Optional<Product> order = repository.findById(id);
-		return order.get();
+		return order.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Product insert(Product obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public Product update(Long id, Product obj) {
+		
+		try {
+			Product entity = repository.getById(id);
+			updateData(entity, obj); 
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Product entity, Product obj) {
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+		entity.setImgUrl(obj.getImgUrl());
 	}
 	
 }
