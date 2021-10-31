889c4f0d8085cd9ab1716c0e67daf20afb1556cf
Jonathan Silva <jonathanfelsilva@gmail.com>
2021-10-31 16:10:33 -0300

    Post, Delete e Put da entidade Categoria

--- src/main/java/com/jonathan/course/resources/CategoryResource.java
+++ src/main/java/com/jonathan/course/resources/CategoryResource.java
@@ -1,15 +1,22 @@
 package com.jonathan.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jonathan.course.entities.Category;
+import com.jonathan.course.entities.User;
 import com.jonathan.course.services.CategoryService;
 
 @RestController
@@ -30,5 +37,26 @@ public class CategoryResource {
 		Category category = service.findById(id);
 		return ResponseEntity.ok().body(category);
 	}
-
+		
+	@PostMapping
+	public ResponseEntity<Category> insert(@RequestBody Category obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+	
 }
